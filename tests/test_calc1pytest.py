e9a442c84ac7c290b0fe4314240425a2cba7f19c
Łukasz Arndt <lukarn@gmail.com>
2019-09-26 20:31:35 +0200

    import external package

--- tests/test_calc1pytest.py
+++ tests/test_calc1pytest.py
@@ -4,6 +4,8 @@ import time
 import pytest
 from appium import webdriver
 
+import commons.methods
+
 def setup_module(module, iii=int):
     """ setup any state specific to the execution of the given module."""
     print("\n===========test suite beginning=========================================")
@@ -26,31 +28,31 @@ wSurname = "Testerka" + str(random.randint(1, 1000))
 mName = "Tadek" + str(random.randint(1, 1000))
 mSurname = "Tester" + str(random.randint(1, 1000))
 
-def randomPesel(gender):
-    # birth date
-    p1 = random.randint(3, 9)
-    p2 = random.randint(0, 9)
-    p3 = random.randint(0, 2)
-    p4 = random.randint(1, 9)
-    p5 = random.randint(0, 1)
-    p6 = random.randint(1, 7)
-    pesel = str(p1) + str(p2) + str(p3) + str(p4) + str(p5) + str(p6)
-    # + random number 000 - 999
-    p7 = random.randint(0, 9)
-    p8 = random.randint(0, 9)
-    p9 = random.randint(0, 9)
-    pesel = pesel + str(p7) + str(p8) + str(p9)
-    # + gender
-    if gender == "w":
-        items = [0, 2, 4, 6, 8]
-    else:
-        items = [1, 3, 5, 7, 9]
-    p10 = items[random.randrange(len(items))]
-    pesel = pesel + str(p10)
-    p11 = (9 * p1) + (7 * p2) + (3 * p3) + (1 * p4) + (9 * p5) + (7 * p6) + (3 * p7) + (1 * p8) + (9 * p9) + (7 * p10)
-    p11 = p11 % 10
-    pesel = pesel + str(p11)
-    return pesel
+# def randomPesel(gender):
+#     # birth date
+#     p1 = random.randint(3, 9)
+#     p2 = random.randint(0, 9)
+#     p3 = random.randint(0, 2)
+#     p4 = random.randint(1, 9)
+#     p5 = random.randint(0, 1)
+#     p6 = random.randint(1, 7)
+#     pesel = str(p1) + str(p2) + str(p3) + str(p4) + str(p5) + str(p6)
+#     # + random number 000 - 999
+#     p7 = random.randint(0, 9)
+#     p8 = random.randint(0, 9)
+#     p9 = random.randint(0, 9)
+#     pesel = pesel + str(p7) + str(p8) + str(p9)
+#     # + gender
+#     if gender == "w":
+#         items = [0, 2, 4, 6, 8]
+#     else:
+#         items = [1, 3, 5, 7, 9]
+#     p10 = items[random.randrange(len(items))]
+#     pesel = pesel + str(p10)
+#     p11 = (9 * p1) + (7 * p2) + (3 * p3) + (1 * p4) + (9 * p5) + (7 * p6) + (3 * p7) + (1 * p8) + (9 * p9) + (7 * p10)
+#     p11 = p11 % 10
+#     pesel = pesel + str(p11)
+#     return pesel
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
@@ -76,7 +78,7 @@ class TestClass:
     def test_1addition(self):
         print("\n", wName, wSurname, "\t", mName, mSurname, "\n")
         for i in range(110):
-            print(randomPesel("w"))
+            print(commons.methods.randomPesel("w"))
         driver.find_element_by_name("Jeden").click()
         driver.find_element_by_name("Plus").click()
         driver.find_element_by_name("Dwa").click()
